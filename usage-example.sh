6512ed87540b15b4f935df0aa752725e6d9a8aad
ViniBR01 <vinicius@rice.edu>
2021-04-16 11:08:54 -0500

    First version of the usage-example

--- usage-example.sh
+++ usage-example.sh
@@ -1,3 +1,22 @@
 #! /bin/bash
 
 # Example of usage to guide development and future usage
+
+# Set parameters (fixed or an array of values)
+filesize = 300000
+traffic_load = (0.1 0.2 0.3 0.5 0.7 0.9)
+upload_ratio = 0.5
+
+MCS = 8
+uplink_mode = (1 4 5)
+AP_antennas = 4
+max_aggregation = 40000
+AP_priority = 4
+
+#Call script with all parameters
+echo "Start of all experiments."
+./example_main filesize traffic_load upload_ratio MCS uplink_mode AP_antennas max_aggregation AP_priority
+
+wait
+
+echo "End of all experiments."
