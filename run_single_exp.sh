a73e083e13e6dcae6ee98d730f493d67feaf6c7d
ViniBR01 <vinicius@rice.edu>
2021-04-18 17:57:54 -0500

    Write example call for simultaneous traffic

--- run_single_exp.sh
+++ run_single_exp.sh
@@ -12,13 +12,16 @@ max_aggregation=$5
 AP_antennas=$6
 uplink_mode=$7
 traffic_load=$8
-dir_name=$9
+path=$9
 
 echo "Running single experiment: $filesize $AP_priority $upload_ratio \
-$MCS $max_aggregation $AP_antennas $uplink_mode $traffic_load $dir_name"
+$MCS $max_aggregation $AP_antennas $uplink_mode $traffic_load $path"
 sudo -u vini-desktop mkdir -p $dir_name
 exec /home/vini-desktop/src/libs/libnetfilter_queue/examples/nf-queue 0 8 1 4 40000 &
 #sudo -u vini-desktop ./run_experiment_Mix.sh
-echo "Test running 32 stations..."
+#echo "Test running 32 stations..."
+#fix-me: call script that runs 32 simultaneous stations
+#e.g., 
+#sudo -u vini-desktop ./run_simult_32 $size, $priority, $ratio, $mcs, $aggreg, $antenna, $mode, $load, $path
 pkill nf-queue
 
