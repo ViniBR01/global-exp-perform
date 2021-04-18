44ccdcdecae09a24ed51fcf1122caa8d2555f9f0
ViniBR01 <vinicius@rice.edu>
2021-04-18 17:23:40 -0500

    Run and kill PERFORM for a single exp

--- run_single_exp.sh
+++ run_single_exp.sh
@@ -3,3 +3,8 @@
 # Script to run single experiment given all parameters
 # For usage, see usage-example.sh. Do not modify this file for new experiments.
 
+echo "Running single experiment: 'enter parameters here'"
+exec /home/vini-desktop/src/libs/libnetfilter_queue/examples/nf-queue 0 8 1 4 40000 &
+#sudo -u vini-desktop ./run_experiment_Mix.sh
+pkill nf-queue
+
