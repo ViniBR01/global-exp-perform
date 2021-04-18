06c0556a39a69f9263a30e270ef91d81a2383822
ViniBR01 <vinicius@rice.edu>
2021-04-18 10:43:09 -0500

    Add more array options to the example

--- usage-example.sh
+++ usage-example.sh
@@ -6,8 +6,8 @@
 # use semicolon to separate values in a string array
 
 filesize='300000'
-traffic_load='0.1;0.3;0.5'
-upload_ratio='0.5'
+traffic_load='10;30;50;70'
+upload_ratio='0.0;0.5;1.0'
 MCS='8'
 uplink_mode='4;5'
 AP_antennas='4'
