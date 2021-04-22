ad5586a6ac125f4386845ebc536a07291991c62f
ViniBR01 <vinicius@rice.edu>
2021-04-22 13:39:00 -0500

    Pass two more parameters to python

--- run_simult_32.sh
+++ run_simult_32.sh
@@ -36,40 +36,40 @@ runtime=$length
 # exit
 
 echo "Experiment running 32 stations..."
-python3 tcp.py -i 192.168.0.102 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta01.txt" &
-python3 tcp.py -i 192.168.0.103 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta02.txt" &
-python3 tcp.py -i 192.168.0.104 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta03.txt" &
-python3 tcp.py -i 192.168.0.105 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta04.txt" &
-python3 tcp.py -i 192.168.0.106 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta05.txt" &
-python3 tcp.py -i 192.168.0.107 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta06.txt" &
+python3 tcp.py -i 192.168.0.102 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta01.txt" &
+python3 tcp.py -i 192.168.0.103 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta02.txt" &
+python3 tcp.py -i 192.168.0.104 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta03.txt" &
+python3 tcp.py -i 192.168.0.105 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta04.txt" &
+python3 tcp.py -i 192.168.0.106 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta05.txt" &
+python3 tcp.py -i 192.168.0.107 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta06.txt" &
 
-python3 tcp.py -i 192.168.0.202 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta07.txt" &
-python3 tcp.py -i 192.168.0.203 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta08.txt" &
-python3 tcp.py -i 192.168.0.204 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta09.txt" &
-python3 tcp.py -i 192.168.0.205 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta10.txt" &
-python3 tcp.py -i 192.168.0.206 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta11.txt" &
-python3 tcp.py -i 192.168.0.207 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta12.txt" &
+python3 tcp.py -i 192.168.0.202 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta07.txt" &
+python3 tcp.py -i 192.168.0.203 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta08.txt" &
+python3 tcp.py -i 192.168.0.204 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta09.txt" &
+python3 tcp.py -i 192.168.0.205 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta10.txt" &
+python3 tcp.py -i 192.168.0.206 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta11.txt" &
+python3 tcp.py -i 192.168.0.207 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta12.txt" &
 
-python3 tcp.py -i 192.168.0.132 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta13.txt" &
-python3 tcp.py -i 192.168.0.133 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta14.txt" &
-python3 tcp.py -i 192.168.0.134 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta15.txt" &
-python3 tcp.py -i 192.168.0.135 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta16.txt" &
-python3 tcp.py -i 192.168.0.136 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta17.txt" &
-python3 tcp.py -i 192.168.0.137 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta18.txt" &
-python3 tcp.py -i 192.168.0.138 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta19.txt" &
-python3 tcp.py -i 192.168.0.139 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta20.txt" &
-python3 tcp.py -i 192.168.0.140 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta21.txt" &
-python3 tcp.py -i 192.168.0.141 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta22.txt" &
+python3 tcp.py -i 192.168.0.132 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta13.txt" &
+python3 tcp.py -i 192.168.0.133 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta14.txt" &
+python3 tcp.py -i 192.168.0.134 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta15.txt" &
+python3 tcp.py -i 192.168.0.135 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta16.txt" &
+python3 tcp.py -i 192.168.0.136 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta17.txt" &
+python3 tcp.py -i 192.168.0.137 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta18.txt" &
+python3 tcp.py -i 192.168.0.138 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta19.txt" &
+python3 tcp.py -i 192.168.0.139 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta20.txt" &
+python3 tcp.py -i 192.168.0.140 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta21.txt" &
+python3 tcp.py -i 192.168.0.141 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta22.txt" &
 
-python3 tcp.py -i 192.168.0.162 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta23.txt" &
-python3 tcp.py -i 192.168.0.163 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta24.txt" &
-python3 tcp.py -i 192.168.0.164 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta25.txt" &
-python3 tcp.py -i 192.168.0.165 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta26.txt" &
-python3 tcp.py -i 192.168.0.166 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta27.txt" &
-python3 tcp.py -i 192.168.0.167 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta28.txt" &
-python3 tcp.py -i 192.168.0.168 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta29.txt" &
-python3 tcp.py -i 192.168.0.169 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta30.txt" &
-python3 tcp.py -i 192.168.0.170 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta31.txt" &
-python3 tcp.py -i 192.168.0.171 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio > "${path}result-sta32.txt" &
+python3 tcp.py -i 192.168.0.162 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta23.txt" &
+python3 tcp.py -i 192.168.0.163 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta24.txt" &
+python3 tcp.py -i 192.168.0.164 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta25.txt" &
+python3 tcp.py -i 192.168.0.165 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta26.txt" &
+python3 tcp.py -i 192.168.0.166 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta27.txt" &
+python3 tcp.py -i 192.168.0.167 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta28.txt" &
+python3 tcp.py -i 192.168.0.168 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta29.txt" &
+python3 tcp.py -i 192.168.0.169 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta30.txt" &
+python3 tcp.py -i 192.168.0.170 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta31.txt" &
+python3 tcp.py -i 192.168.0.171 -f $filesize -l $runtime -t $traffic_load -m $upload_ratio -r $MCS -a $AP_antennas > "${path}result-sta32.txt" &
 
 wait
